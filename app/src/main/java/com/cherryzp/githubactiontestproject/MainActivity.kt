6db2584916a14cbe63c7c50bd5a44ffdb6b3a2df
cherryzp <cherryzp9094@gmail.com>
2022-02-13 19:12:28 +0900

    action test Commit

--- app/src/main/java/com/cherryzp/githubactiontestproject/MainActivity.kt
+++ app/src/main/java/com/cherryzp/githubactiontestproject/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.cherryzp.githubactiontestproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun checkNum(any: Any): Boolean {
+        if (any is Int) {
+            return true
+        }
+        return false
     }
 }
